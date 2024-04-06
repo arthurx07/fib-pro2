e7c886e3f4859be024bf64df632297bc6dec12b3
kumao <arturleivarx@gmail.com>
2024-04-07 00:52:44 +0200

    sparse matrices: implement product :)

--- S4 - list_set_map/sparse_matrices.cc
+++ S4 - list_set_map/sparse_matrices.cc
@@ -98,30 +98,101 @@ void sum(const std::vector<std::list<std::pair<int, T>>>& a,
   }
 }
 
-// void product();
+/* Pre: A and B are two sparse matrices of orders a1xa2 and 
+ *  b1xb2. Where a2 = b1 (the number of columns of A is equal
+ *  to the number of rows of B) */
+/* Post: C is AB, of dimension a1xb2 */
+template <typename T>
+void product(const std::vector<std::list<std::pair<int, T>>>& a,
+             const std::vector<std::list<std::pair<int, T>>>& b,
+             std::vector<std::list<std::pair<int, T>>>& c, int bcol)
+{
+  // assert (acol == b.size())
+  c = std::vector<std::list<std::pair<int, T>>> (a.size());
+
+  for (int i = 0; i < c.size(); ++i) // c.size() == a.size() == rows of C
+  {
+    typename std::list<std::pair<int, T>>::iterator itc = c[i].begin();
+ 
+    // vector of iterators of matrix B
+    std::vector< typename std::list<std::pair<int, T>>::const_iterator > vitb(b.size()); // b.size() == rows of B == columns of A
+    for (int idx = 0; idx < b.size(); ++idx) vitb[idx] = b[idx].begin(); // reset all iterators of vector<iterator> to begin()
+
+    for (int j = 0; j < bcol; ++j) // c[0].size() == b[0].size == bcol == columns of C
+    {
+      T value = 0;
+      typename std::list<std::pair<int, T>>::const_iterator ita = a[i].begin();
+      for (int k = 0; k < b.size(); ++k) { // b.size() == a[0].size() == rows of B == columns of A
+        bool a_empty = not (ita != a[i].end() and ita->first == k);
+        bool b_empty = not (vitb[k] != b[k].end() and vitb[k]->first == j);
+
+        if (not a_empty and not b_empty)
+          value += (ita->second)*(vitb[k]->second);
+        if (not a_empty) ++ita;
+        if (not b_empty) ++vitb[k];
+      }
+      if (value != 0) c[i].insert(c[i].end(), std::make_pair(j, value));
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
-  if (argc != 3)
+  if (argc < 2)
   {
-    std::cout << "Error: parameters row col were expected but not found" << std::endl;
+    std::cout << "Error: parameters sum or product were expected but not found." << std::endl;
     exit(1);
   }
-  int row = atoi(argv[1]);
-  int col = atoi(argv[2]);
-  std::cout << "Write a matrix A and B of dimensions " << row << 'x' << col << '.' << std::endl;
+  else if (std::string(argv[1]) == "sum")
+  {
+    if (argc != 4)
+    {
+      std::cout << "Error: parameters row col were expected but not found." << std::endl;
+      exit(1);
+    }
+    int row = atoi(argv[2]);
+    int col = atoi(argv[3]);
+    std::cout << "Write a matrix A and B of dimensions " << row << 'x' << col << '.' << std::endl;
+    
+    std::vector< std::list<std::pair<int, int>> > a(row);
+    // std::vector<std::list<std::pair<int, double>>> a(row);
+    read_matrix(a, row, col);
+    std::vector< std::list<std::pair<int, int>> > b(row);
+    read_matrix(b, row, col);
 
-  std::vector< std::list<std::pair<int, int>> > a(row);
-  // std::vector<std::list<std::pair<int, double>>> a(row);
-  read_matrix(a, row, col);
-  std::vector< std::list<std::pair<int, int>> > b(row);
-  read_matrix(b, row, col);
+    // std::cout << std::endl << "The written matrix is:" << std::endl;
+    // write_matrix(a, col);
 
-  // std::cout << std::endl << "The written matrix is:" << std::endl;
-  // write_matrix(a, col);
+    std::vector< std::list<std::pair<int, int>> > c(row);
+    sum(a, b, c, col);
+    std::cout << std::endl << "The sum matrix A+B is:" << std::endl;
+    write_matrix(c, col);
+  }
+  else if (std::string(argv[1]) == "product")
+  {
+    int row, col;
 
-  std::vector< std::list<std::pair<int, int>> > c(row);
-  sum(a, b, c, col);
-  std::cout << std::endl << "The sum matrix A+B is:" << std::endl;
-  write_matrix(c, col);
+    std::cout << "Write the dimensions of the matrix A: " << std::endl;
+    std::cout << "  number of rows: ";
+    std::cin >> row;
+    std::cout << "  number of columns: ";
+    std::cin >> col;
+    std::vector< std::list<std::pair<int, int>> > a(row);
+    // std::vector<std::list<std::pair<int, double>>> a(row);
+    read_matrix(a, row, col);
+
+    std::cout << "Write the dimensions of the matrix A: " << std::endl;
+    std::cout << "  number of rows: ";
+    std::cin >> row;
+    std::cout << "  number of columns: ";
+    std::cin >> col;
+    std::vector< std::list<std::pair<int, int>> > b(row);
+    // std::vector<std::list<std::pair<int, double>>> b(row);
+    read_matrix(b, row, col);
+
+    std::vector< std::list<std::pair<int, int>> > c(row);
+    product(a, b, c, col);
+    std::cout << std::endl << "The product matrix AB is:" << std::endl;
+    write_matrix(c, col);
+  }
 }
